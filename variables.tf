b1eefa6d62d2ff86cca9755406cb0309061d43f4
Chris Kendrick <kendrickcc@gmail.com>
2022-03-06 16:52:52 -0600

    added other sizes

--- variables.tf
+++ variables.tf
@@ -14,6 +14,9 @@ variable "pub_key_loc" {
 variable "region" {
   default = "myc3"
 }
+variable "webodm_cidr" {
+  default = "192.168.10.0/24"
+}
 variable "webodm_count" {
   description = "Number of WebODM consoles"
   default     = 1
@@ -24,8 +27,26 @@ variable "webodm_os" {
 }
 variable "webodm_size" {
   description = "The server size"
-  default     = "s-1vcpu-1gb"
+  default     = "s-4vcpu-8gb"
 }
-variable "webodm_cidr" {
-  default = "192.168.10.0/24"
-}
+/*
+Slug                 Memory    VCPUs    Disk    Price Monthly    Price Hourly
+s-1vcpu-1gb          1024      1        25      5.00             0.007440
+s-1vcpu-1gb-amd      1024      1        25      6.00             0.008930
+s-1vcpu-1gb-intel    1024      1        25      6.00             0.008930
+s-1vcpu-2gb          2048      1        50      10.00            0.014880
+s-1vcpu-2gb-amd      2048      1        50      12.00            0.017860
+s-1vcpu-2gb-intel    2048      1        50      12.00            0.017860
+s-2vcpu-2gb          2048      2        60      15.00            0.022320
+s-2vcpu-2gb-amd      2048      2        60      18.00            0.026790
+s-2vcpu-2gb-intel    2048      2        60      18.00            0.026790
+s-2vcpu-4gb          4096      2        80      20.00            0.029760
+s-2vcpu-4gb-amd      4096      2        80      24.00            0.035710
+s-2vcpu-4gb-intel    4096      2        80      24.00            0.035710
+s-4vcpu-8gb          8192      4        160     40.00            0.059520
+c-2                  4096      2        25      40.00            0.059520
+g-2vcpu-8gb          8192      2        25      60.00            0.089290
+gd-2vcpu-8gb         8192      2        50      65.00            0.096730
+m-2vcpu-16gb         16384     2        50      80.00            0.119050
+c-4                  8192      4        50      80.00            0.119050
+*/
