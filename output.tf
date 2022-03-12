ddf1be04ed62fd3cd84e7d071985e612577a5e3a
Chris Kendrick <kendrickcc@gmail.com>
2022-03-11 19:42:16 -0600

    output fix

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "ClusterODM_internal_ip" {
-  value = droplet.webodm.private_ip
+  value = digitalocean_droplet.webodm.ipv4_address_private
 }
 output "NodeODM_internal_ip" {
-  value = droplet.nodeodm.*.public_ip
+  value = digitalocean_droplet.nodeodm.*.ipv4_address_private
 }
 output "WebODM" {
-  value = droplet.webodm.public_ip
+  value = digitalocean_droplet.webodm.ipv4_address 
 }
